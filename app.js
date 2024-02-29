b5a8804250df4b092ba22b043507314efb941b05
Jean-Philippe Jutras <73455167+jpjutras82@users.noreply.github.com>
2024-02-29 10:41:50 -0500

    FTP Connexion v1.1

--- app.js
+++ app.js
@@ -9,83 +9,20 @@ const path = require('path');
 const imagesPath = path.join(__dirname, 'images');
 const ftp = require('ftp');
 const fs = require('fs');
+const AdmZip = require('adm-zip');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-// ======================================================
-// FTP CONNEXION (dependencies: node-ftp)
-// FTP Connexion à ftp.solusoft-erp.com
-const client = new ftp();
-const config = {
-  host: 'ftp.solusoft-erp.com',
-  port: 21,
-  user: 'Ricart@solusoft-erp.com',
-  password: 'ric2024art'
-};
-
-client.connect(config);
-
-
-// FTP Listing du contenu 
-client.on('ready', () => {
-  client.list((err, list) => {
-    if (err) throw err;
-    console.log('Listing du contenu des dossiers:');
-    console.dir(list);
-    client.end();
-  });
-});
-
-
-// FTP Téléchargement d'un fichier
-client.on('ready', () => {
-  client.list((err, list) => {
-    if (err) throw err;
-    console.log('Listing du contenu des dossiers:');
-    console.dir(list);
-
-    // FTP Téléchargement d'un fichier - Chemin vers le fichier produitTest.zip sur le serveur
-    const remoteFilePath = '/Produits/produitTest.zip';
-    // FTP Téléchargement d'un fichier - Chemin vers le fichier produitTest.zip sur local
-    const localFilePath = 'produitTest666.zip'; // Path where you want to save the downloaded file
-
-    // FTP GET - Méthode pour télécharger un fichier
-    client.get(remoteFilePath, (err, stream) => {
-      if (err) {
-        console.error('Error downloading file:', err);
-        return;
-      }
-
-      // FTP PIPE PROCESS - Stream du fichier Serveur vers Stream Local
-      stream.pipe(fs.createWriteStream(localFilePath));
-
-      // FTP CLOSE EVENT - Écoute de la terminaison du processus de téléchargement
-      stream.once('close', () => {
-        console.log('File downloaded successfully');
-        client.end(); // Close the FTP connection
-      });
-    });
-  });
-});
-
-// FTP Gestion des événements
-client.on('error', (err) => {
-  console.log('FTP error occurred: ' + err);
-});
-
-
-
-
 // Middleware pour ajouter les headers CORS
 const allowedOrigins = ['http://localhost:3000', 'https://api-cheeringstuff.onrender.com'];
 app.use(cors({
   origin: allowedOrigins,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, 
-  optionsSuccessStatus: 204, 
+  credentials: true,
+  optionsSuccessStatus: 204,
 }));
 
 
@@ -153,7 +90,7 @@ app.use(errorController.logErrors);
 app.use(errorController.get404);
 
 
-// Connexion à la base de données
+// MONGODB -Connexion à la base de données
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}/${process.env.DB_NAME}?retryWrites=true&w=majority`)
   .then(() => {
     console.log('La connexion à la base de données est établie, http://localhost:3030');
@@ -165,3 +102,70 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD
   .catch(err => {
     console.log('La connexion à la base de données a échoué', err);
   });
+
+// FTP CONNEXION (dependencies: node-ftp) - Connexion à ftp.solusoft-erp.com
+const client = new ftp();
+const config = {
+  host: 'ftp.solusoft-erp.com',
+  port: 21,
+  user: 'Ricart@solusoft-erp.com',
+  password: 'ric2024art'
+};
+
+client.connect(config);
+
+// FTP Vérification de la connexion + Listing du contenu
+client.on('ready', () => {
+  client.list((err, list) => {
+    if (err) throw err;
+    console.log('Vous êtes bien connecté au serveur FTP.');
+    /*     console.log('Listing du contenu des dossiers:');
+        console.dir(list); */
+    client.end();
+  });
+});
+
+
+// FTP Active l'écoute des événements et déclanche les callbacks.
+client.on('ready', () => {
+  client.list((err, list) => {
+    if (err) throw err;
+    // FTP Sélection du fichier à télécharger sur le serveuer - produitTest.zip (temporaire)
+    const remoteFilePath = '/Produits/produitTest.zip';
+    // FTP Destination du dossier/nom du fichier dans le dossier du projet (local)
+    const localFilePath = 'solusoft/compressedFiles/produitTest666.zip';
+
+    //GET - Méthode pour télécharger un fichier
+    client.get(remoteFilePath, (err, stream) => {
+      if (err) {
+        console.error('Error downloading file:', err);
+        return;
+      }
+
+      // FTP PIPE PROCESS - Stream du fichier Serveur vers Stream Local
+      stream.pipe(fs.createWriteStream(localFilePath));
+
+      // FTP CLOSE EVENT - Écoute de la terminaison du processus de téléchargement
+      stream.once('close', () => {
+        console.log('File downloaded successfully');
+        client.end(); // Close the FTP connection
+      });
+    });
+  });
+});
+
+// FTP Gestion des événements
+client.on('error', (err) => {
+  console.log('FTP error occurred: ' + err);
+});
+
+// ADM-ZIP - Gestionnaire de compression/décompression de fichier
+// ZIP - Chemin d'accès vers le fichier zip (produits)
+const zipFilePath = 'solusoft/compressedFiles/produitTest666.zip/';
+
+// Crée une instance d'AdmZip
+const zip = new AdmZip(zipFilePath);
+
+// Extrait le contenu du fichier compressé (produits)
+zip.extractAllTo('solusoft/uncompressedFiles', true);
+
