2b8d02279da216ac92a6598c656da6f3b90bef73
Jean-Philippe Jutras <73455167+jpjutras82@users.noreply.github.com>
2024-02-29 15:13:57 -0500

    FTP Connexion + Unzip - Gestion de créations des dossiers compressedFiles+uncompressedFiles

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD
     console.log('La connexion à la base de données a échoué', err);
   });
 
-// FTP CONNEXION (dependencies: node-ftp) - Connexion à ftp.solusoft-erp.com
+// FTP CONNEXION (dependencies: node-ftp) - Connexion à ftp.solusoft-erp.com (+ Création des sous-dossiers Solusoft + Décompression des fichiers)
 const client = new ftp();
 const config = {
   host: 'ftp.solusoft-erp.com',
@@ -114,27 +114,54 @@ const config = {
 
 client.connect(config);
 
-// FTP Vérification de la connexion + Listing du contenu
-client.on('ready', () => {
-  client.list((err, list) => {
-    if (err) throw err;
-    console.log('Vous êtes bien connecté au serveur FTP.');
-    /*     console.log('Listing du contenu des dossiers:');
-        console.dir(list); */
-    client.end();
-  });
-});
-
-
 // FTP Active l'écoute des événements et déclanche les callbacks.
 client.on('ready', () => {
   client.list((err, list) => {
     if (err) throw err;
-    // FTP Sélection du fichier à télécharger sur le serveuer - produitTest.zip (temporaire)
-    const remoteFilePath = '/Produits/produitTest.zip';
-    // FTP Destination du dossier/nom du fichier dans le dossier du projet (local)
-    const localFilePath = 'solusoft/compressedFiles/produitTest666.zip';
+    // Ajout du fichier temporaire pour recevoir le WriteStream
 
+    const fs = require('fs');
+    const path = require('path');
+    
+    const folderPath = './folder';
+    
+    // Vérification SI dossier "solusoft/compressedFiles" existe déjà, sinon le créé.
+    fs.access("solusoft/compressedFiles", fs.constants.F_OK, (err) => {
+        if (err) {
+            // Directory does not exist, so create it
+            fs.mkdir("solusoft/compressedFiles", { recursive: true }, (err) => {
+                if (err) {
+                    console.error('Erreur de création du dossier "solusoft/compressedFiles":', err);
+                } else {
+                    console.log('Dossier "solusoft/compressedFiles" créé avec succès"');
+                }
+            });
+        } else {
+            console.log('Le dossier "solusoft/compressedFiles" existe déjà');
+        }
+    });
+
+        // Vérification SI dossier "solusoft/uncompressedFiles" existe déjà, sinon le créé.
+        fs.access("solusoft/uncompressedFiles", fs.constants.F_OK, (err) => {
+          if (err) {
+              // Directory does not exist, so create it
+              fs.mkdir("solusoft/uncompressedFiles", { recursive: true }, (err) => {
+                  if (err) {
+                      console.error('Erreur de création du dossier "solusoft/uncompressedFiles":', err);
+                  } else {
+                      console.log('Dossier "solusoft/uncompressedFiles" créé avec succès"');
+                  }
+              });
+          } else {
+              console.log('Le dossier "solusoft/uncompressedFiles" existe déjà');
+          }
+      });
+    
+
+    const pathName = 'solusoft/compressedFiles/downloadedFile.zip';
+
+    // FTP Sélection du fichier à télécharger sur le serveuer - produitTest.zip (temporaire)
+    const remoteFilePath = '/Produits/produitTest.zip'; // Fichier sur le FTP
     //GET - Méthode pour télécharger un fichier
     client.get(remoteFilePath, (err, stream) => {
       if (err) {
@@ -142,13 +169,27 @@ client.on('ready', () => {
         return;
       }
 
-      // FTP PIPE PROCESS - Stream du fichier Serveur vers Stream Local
-      stream.pipe(fs.createWriteStream(localFilePath));
+      // FTP PIPE PROCESS - WriteStream du fichier Serveur vers Stream Local
+      stream.pipe(fs.createWriteStream(pathName));
+
 
       // FTP CLOSE EVENT - Écoute de la terminaison du processus de téléchargement
       stream.once('close', () => {
-        console.log('File downloaded successfully');
+        console.log('fichier "downloadedFile.zip" téléchargé avec succès');
         client.end(); // Close the FTP connection
+
+      // ADM-ZIP - Gestionnaire de compression/décompression de fichier
+      // ZIP - Chemin d'accès vers le fichier zip (produits)
+      const zipFilePath = 'solusoft/compressedFiles/downloadedFile.zip';
+
+
+      // Crée une instance d'AdmZip
+      const zip = new AdmZip(zipFilePath);
+
+
+      // Extrait le contenu du fichier compressé (produits)
+      zip.extractAllTo('solusoft/uncompressedFiles', true);
+
       });
     });
   });
@@ -159,12 +200,3 @@ client.on('error', (err) => {
   console.log('FTP error occurred: ' + err);
 });
 
-// ADM-ZIP - Gestionnaire de compression/décompression de fichier
-// ZIP - Chemin d'accès vers le fichier zip (produits)
-const zipFilePath = 'solusoft/compressedFiles/produitTest666.zip/';
-
-// Crée une instance d'AdmZip
-const zip = new AdmZip(zipFilePath);
-
-// Extrait le contenu du fichier compressé (produits)
-zip.extractAllTo('solusoft/uncompressedFiles', true);
