538fb061faecdf38985ff282e6e211523f1a04e2
Sebastien Arsenault <sebastienarsenault1999@gmail.com>
2024-03-04 10:45:13 -0500

    removec post, put and delete product

--- routes/routesProduct.js
+++ routes/routesProduct.js
@@ -11,14 +11,4 @@ router.get('/products',   ProductController.getProducts);
 // Route pour obtenir un produit spécifique par son ID (accessible par un utilisateur authentifié)
 router.get('/products/:id', authId, ProductController.getProductById);
 
-// Route pour mettre à jour un produit (accessible par un administrateur)
-router.put('/products/:id', authAdmin,  ProductController.updateProduct);
-
-// Route pour créer un nouveau produit (accessible par un administrateur)
-router.post('/products', authAdmin,  ProductController.createProduct);
-
-// Route pour supprimer un produit (accessible par un administrateur)
-router.delete('/products/:id', authAdmin, ProductController.deleteProduct);
-
-
 module.exports = router;
