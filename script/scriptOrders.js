62d0b042814c6bc813f9a2f6f08d75f3c8ce0e6b
TOUFIK DELLYS <dellystofik9@gmail.com>
2024-03-06 16:19:01 -0500

    metter le code en forme

--- script/scriptOrders.js
+++ script/scriptOrders.js
@@ -5,16 +5,16 @@ const fs = require("fs");
 const Order = require("../models/orders");
 const fsExtra = require("fs-extra");
 
- // Connexion au serveur FTP
+// Connexion au serveur FTP
 const client = new ftp();
 const config = {
   host: "ftp.solusoft-erp.com",
   port: 21,
   user: "Ricart@solusoft-erp.com",
   password: "ric2024art",
-}; 
+};
 
-client.connect(config); 
+client.connect(config);
 
 // Fonction pour convertir une commande en fichier JSON
 const orderToJSON = (order) => {
@@ -49,7 +49,7 @@ client.on("ready", () => {
     console.dir(list);
     client.end();
   });
-}); 
+});
 
 // Fonction pour importer les commandes depuis MongoDB et les convertir en fichiers JSON.
 exports.importOrders = async () => {
@@ -58,7 +58,7 @@ exports.importOrders = async () => {
     const orders = await Order.find();
 
     // Créer le répertoire 'orders/' s'il n'existe pas
-     const directoryPath = "solusoft/parseOrdersFiles"; 
+    const directoryPath = "solusoft/parseOrdersFiles";
 
     if (!fs.existsSync(directoryPath)) {
       fs.mkdirSync(directoryPath, { recursive: true });
@@ -98,7 +98,7 @@ exports.exportOrdersToFTP = async () => {
 
     // Créer le répertoire distant "Commandes"
     await createRemoteDirectory(remoteDirectory);
-    
+
     //// Parcourir les commandes et les exporter vers le serveur FTP
     for (const order of orders) {
       const directoryPath = "solusoft/parseOrdersFiles";
@@ -114,12 +114,10 @@ exports.exportOrdersToFTP = async () => {
 
       // Écrire le fichier JSON temporairement localement
       fs.writeFileSync(localFilePathForUpload, orderJSON);
- 
-  
-    console.log(client.connected); 
+
+      console.log(client.connected);
       // Téléverser le fichier JSON vers le serveur FTP
       if (fs.existsSync(localFilePathForUpload) && client.connected) {
-        
         client.put(
           localFilePathForUpload,
           `${remoteDirectory}/${fileName}`,
@@ -132,13 +130,11 @@ exports.exportOrdersToFTP = async () => {
               return;
             }
             console.log(`${fileName} a été téléversé avec succès.`);
-            // Supprimer le fichier JSON temporaire localement
-            //fsExtra.unlinkSync(localFilePath);
           }
         );
       } else {
         console.error(`${fileName} n'existe pas.`);
-      } 
+      }
     }
   } catch (error) {
     console.error(
@@ -147,7 +143,6 @@ exports.exportOrdersToFTP = async () => {
     );
   } finally {
     // Fermer la connexion FTP
-     client.end();
-     
+    client.end();
   }
 };
